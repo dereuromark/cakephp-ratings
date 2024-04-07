c7db3e9150edf84aba637451bed5b462e84130ec
mscherer <dereuromark@web.de>
2024-04-07 20:05:01 +0200

    Fix up tests for postgres.

--- tests/Fixture/RatingsFixture.php
+++ tests/Fixture/RatingsFixture.php
@@ -28,11 +28,11 @@ class RatingsFixture extends TestFixture {
 	public array $fields = [
 		'id' => ['type' => 'integer', 'null' => false],
 		'user_id' => ['type' => 'integer', 'null' => true, 'default' => null],
-		'foreign_key' => ['type' => 'string', 'null' => true, 'default' => null],
-		'model' => ['type' => 'string', 'null' => true, 'default' => null],
-		'value' => ['type' => 'float', 'null' => true, 'default' => '0', 'length' => '8,4'],
-		'created' => ['type' => 'datetime', 'null' => true, 'default' => null],
-		'modified' => ['type' => 'datetime', 'null' => true, 'default' => null],
+		'foreign_key' => ['type' => 'integer', 'null' => false, 'default' => null],
+		'model' => ['type' => 'string', 'null' => false, 'default' => null],
+		'value' => ['type' => 'float', 'null' => false, 'default' => '0', 'length' => '8,4'],
+		'created' => ['type' => 'datetime', 'null' => false, 'default' => null],
+		'modified' => ['type' => 'datetime', 'null' => false, 'default' => null],
 		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']], 'UNIQUE_RATING' => ['type' => 'unique', 'columns' => ['user_id', 'foreign_key', 'model']]],
 	];
 
