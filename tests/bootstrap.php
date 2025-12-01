59a9f0ff1d34f0b24a23707c0af2e6bbe83e3c6e
Mark <dereuromark@users.noreply.github.com>
2025-12-01 17:32:30 +0000

    Auto-fix CS - CS master branch

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,8 @@ use Cake\Routing\Router;
 use Cake\TestSuite\Fixture\SchemaLoader;
 use Ratings\RatingsPlugin;
 use Shim\Filesystem\Folder;
+use TestApp\Model\Entity\User;
+use TestApp\Model\Table\UsersTable;
 
 if (!defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
@@ -96,5 +98,5 @@ if (env('FIXTURE_SCHEMA_METADATA')) {
 	$loader->loadInternalFile(env('FIXTURE_SCHEMA_METADATA'));
 }
 
-class_alias(\TestApp\Model\Entity\User::class, 'App\Model\Entity\User');
-class_alias(\TestApp\Model\Table\UsersTable::class, 'App\Model\Table\UsersTable');
+class_alias(User::class, 'App\Model\Entity\User');
+class_alias(UsersTable::class, 'App\Model\Table\UsersTable');
