1ca66f52345816ea4eb50302cfdf1b5622634cbd
Mark Scherer <dereuromark@users.noreply.github.com>
2026-05-27 15:10:42 +0200

    Document polymorphic rating key types (#27)
    * Document polymorphic rating key types
    
    * Align integer defaults with polymorphic config

--- src/Model/Entity/Rating.php
+++ src/Model/Entity/Rating.php
@@ -10,7 +10,7 @@ use Cake\ORM\Entity;
  * @property int $id
  * @property int $user_id
  * @property string $model
- * @property int $foreign_key
+ * @property int|string $foreign_key
  * @property float $value
  * @property \Cake\I18n\Time $created
  * @property \Cake\I18n\Time $modified
